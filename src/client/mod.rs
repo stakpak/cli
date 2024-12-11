da74e8abc1c74db0df840b1a987a54ac51c9967f
George <george@stakpak.dev>
2024-12-11 15:58:16 +0200

    Add clone subcommand

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -34,17 +34,106 @@ impl Client {
         })
     }
 
-    pub async fn get_my_account(&self) -> Result<GetMyAccountResponse, ReqwestError> {
+    pub async fn get_my_account(&self) -> Result<GetMyAccountResponse, String> {
         let url = format!("{}/account", self.base_url);
-        self.client.get(&url).send().await?.json().await
+
+        let value: serde_json::Value = self
+            .client
+            .get(&url)
+            .send()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?
+            .json()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?;
+        match serde_json::from_value::<GetMyAccountResponse>(value.clone()) {
+            Ok(response) => Ok(response),
+            Err(e) => {
+                eprintln!("Failed to deserialize response: {}", e);
+                eprintln!("Raw response: {}", value);
+                Err("Failed to deserialize response:".into())
+            }
+        }
     }
 
-    pub async fn list_flows(&self, owner_name: &str) -> Result<GetFlowsResponse, ReqwestError> {
+    pub async fn list_flows(&self, owner_name: &str) -> Result<GetFlowsResponse, String> {
         let url = format!("{}/flows/{}", self.base_url, owner_name);
-        self.client.get(&url).send().await?.json().await
+
+        let value: serde_json::Value = self
+            .client
+            .get(&url)
+            .send()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?
+            .json()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?;
+        match serde_json::from_value::<GetFlowsResponse>(value.clone()) {
+            Ok(response) => Ok(response),
+            Err(e) => {
+                eprintln!("Failed to deserialize response: {}", e);
+                eprintln!("Raw response: {}", value);
+                Err("Failed to deserialize response:".into())
+            }
+        }
+    }
+
+    pub async fn get_flow(
+        &self,
+        owner_name: &str,
+        flow_name: &str,
+    ) -> Result<GetFlowResponse, String> {
+        let url = format!("{}/flows/{}/{}", self.base_url, owner_name, flow_name);
+
+        let value: serde_json::Value = self
+            .client
+            .get(&url)
+            .send()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?
+            .json()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?;
+        match serde_json::from_value::<GetFlowResponse>(value.clone()) {
+            Ok(response) => Ok(response),
+            Err(e) => {
+                eprintln!("Failed to deserialize response: {}", e);
+                eprintln!("Raw response: {}", value);
+                Err("Failed to deserialize response:".into())
+            }
+        }
+    }
+
+    pub async fn get_flow_documents(
+        &self,
+        owner_name: &str,
+        flow_name: &str,
+        version_ref: &str,
+    ) -> Result<GetFlowDocumentsResponse, String> {
+        let url = format!(
+            "{}/flows/{}/{}/{}/documents",
+            self.base_url, owner_name, flow_name, version_ref
+        );
+
+        let value: serde_json::Value = self
+            .client
+            .get(&url)
+            .send()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?
+            .json()
+            .await
+            .map_err(|e: ReqwestError| e.to_string())?;
+        match serde_json::from_value::<GetFlowDocumentsResponse>(value.clone()) {
+            Ok(response) => Ok(response),
+            Err(e) => {
+                eprintln!("Failed to deserialize response: {}", e);
+                eprintln!("Raw response: {}", value);
+                Err("Failed to deserialize response:".into())
+            }
+        }
     }
 }
-
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct GetMyAccountResponse {
     pub username: String,
@@ -53,10 +142,63 @@ pub struct GetMyAccountResponse {
     pub last_name: String,
 }
 
+impl GetMyAccountResponse {
+    pub fn to_text(&self) -> String {
+        format!(
+            "ID: {}\nUsername: {}\nName: {} {}",
+            self.id, self.username, self.first_name, self.last_name
+        )
+    }
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct GetFlowsResponse {
     pub results: Vec<Flow>,
 }
+
+impl GetFlowsResponse {
+    pub fn to_text(&self) -> String {
+        let mut output = String::new();
+
+        for flow in &self.results {
+            let latest_version = flow
+                .versions
+                .iter()
+                .max_by_key(|v| v.created_at)
+                .unwrap_or_else(|| &flow.versions[0]);
+            let tags = latest_version
+                .tags
+                .iter()
+                .map(|t| t.name.clone())
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            output.push_str(&format!(
+                "{:<20} {:<40} {:<10} {:<20} {:<40}\n",
+                flow.name,
+                latest_version.id,
+                format!("{:?}", flow.visibility),
+                latest_version.created_at.format("\"%Y-%m-%d %H:%M UTC\""),
+                if tags.is_empty() { "-" } else { &tags }
+            ));
+        }
+
+        output
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GetFlowResponse {
+    pub permission: GetFlowPermission,
+    pub resource: Flow,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GetFlowPermission {
+    pub read: bool,
+    pub write: bool,
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Flow {
     pub id: Uuid,
@@ -91,4 +233,16 @@ pub struct FlowTag {
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct FlowVersionRelation {
     pub id: Uuid,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetFlowDocumentsResponse {
+    pub documents: Vec<Document>,
+    pub additional_documents: Vec<Document>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Document {
+    pub content: String,
+    pub uri: String,
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -19,14 +19,20 @@ pub enum Commands {
 
     /// List my flows
     List,
+
+    /// Clone a Stakpak project
+    Clone {
+        flow_name: String,
+        flow_version: Option<String>,
+        /// Destination directory
+        #[arg(long, short)]
+        dir: Option<String>,
+    },
     // /// Deploy your app
     // Deploy,
 
     // /// Import existing configurations
     // Import,
-
-    // /// Clone a Stakpak project
-    // Clone,
 }
 
 impl Commands {
@@ -57,7 +63,7 @@ impl Commands {
             Commands::Account => {
                 if let Ok(client) = Client::new(&config) {
                     match client.get_my_account().await {
-                        Ok(data) => println!("{}", serde_json::to_string_pretty(&data).unwrap()),
+                        Ok(data) => println!("{}", data.to_text()),
                         Err(e) => eprintln!("Failed to fetch account {}", e),
                     };
                 }
@@ -72,22 +78,82 @@ impl Commands {
                         }
                     };
                     match client.list_flows(&owner_name).await {
-                        Ok(data) => println!("{}", serde_json::to_string_pretty(&data).unwrap()),
+                        Ok(data) => println!("{}", data.to_text()),
                         Err(e) => eprintln!("Failed to fetch account {}", e),
                     };
                 }
-            } // Commands::Deploy => {
-              //     println!("Deploying...");
-              //     // Add deploy logic here
-              // }
-              // Commands::Import => {
-              //     println!("Importing...");
-              //     // Add import logic here
-              // }
-              // Commands::Clone => {
-              //     println!("Cloning...");
-              //     // Add clone logic here
-              // }
+            }
+            Commands::Clone {
+                flow_name,
+                flow_version,
+                dir,
+            } => {
+                if let Ok(client) = Client::new(&config) {
+                    let owner_name = match client.get_my_account().await {
+                        Ok(data) => data.username,
+                        Err(e) => {
+                            eprintln!("Failed to fetch account {}", e);
+                            return;
+                        }
+                    };
+
+                    let version = match flow_version {
+                        Some(flow_version) => flow_version,
+                        None => {
+                            let res = match client.get_flow(&owner_name, &flow_name).await {
+                                Ok(data) => data,
+                                Err(e) => {
+                                    eprintln!("Failed to fetch flow {}", e);
+                                    return;
+                                }
+                            };
+                            let latest_version = res
+                                .resource
+                                .versions
+                                .iter()
+                                .max_by_key(|v| v.created_at)
+                                .unwrap_or_else(|| &res.resource.versions[0]);
+                            latest_version.id.to_string()
+                        }
+                    };
+
+                    let documents = match client
+                        .get_flow_documents(&owner_name, &flow_name, &version)
+                        .await
+                    {
+                        Ok(data) => data,
+                        Err(e) => {
+                            eprintln!("Failed to fetch documents {}", e);
+                            return;
+                        }
+                    };
+
+                    let base_dir = dir.unwrap_or_else(|| ".".into());
+
+                    for doc in documents
+                        .documents
+                        .into_iter()
+                        .chain(documents.additional_documents)
+                    {
+                        let path = doc.uri.strip_prefix("file:///").unwrap_or(&doc.uri);
+                        let full_path = std::path::Path::new(&base_dir).join(path);
+
+                        // Create parent directories if they don't exist
+                        if let Some(parent) = full_path.parent() {
+                            std::fs::create_dir_all(parent).unwrap_or_else(|e| {
+                                eprintln!("Failed to create directory {}: {}", parent.display(), e);
+                            });
+                        }
+
+                        // Write the files
+                        std::fs::write(&full_path, doc.content).unwrap_or_else(|e| {
+                            eprintln!("Failed to write file {}: {}", full_path.display(), e);
+                        });
+                    }
+
+                    println!("Successfully cloned flow to \"{}\"", base_dir);
+                }
+            }
         }
     }
 }
