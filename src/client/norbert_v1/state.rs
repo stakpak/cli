58ec1723351d57d2307081d8280da6ec65bf3913
Ahmed Hesham Abdelkader <23265119+ahmedhesham6@users.noreply.github.com>
2025-02-17 01:37:53 +0200

    Fix: Infinite Loop - Check if the input is not the same as previous input - Only skip not pending actions

--- src/client/norbert_v1/state.rs
+++ src/client/norbert_v1/state.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Clone, Default)]
+#[derive(Debug, Deserialize, Serialize, Clone, Default, PartialEq)]
 pub struct Scratchpad {
     /// Name of the application being deployed
     #[serde(skip_serializing_if = "Option::is_none")]
